0011956584e7887b3bd391b654d1ed05a3f66e5b
FrancescoPa96 <43400908+FrancescoPa96@users.noreply.github.com>
2020-11-18 19:03:08 +0100

    Primo commit definitivo

--- TwitterCapture.py
+++ TwitterCapture.py
@@ -9,23 +9,17 @@ from pymongo import MongoClient
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from deep_translator import MyMemoryTranslator
 
-### Formattazione Retweet
-def format_tweet(string):
-    string = string.replace('RT', '')
-    string = string.replace("\n", " ")
-    return string
 
 ### Formattazione Tweets
 def cleanTweet(string):
     string = string.replace('#', '')
     string = string.replace('\n', '')
-    string = string.replace('@', '')
+    string = re.sub('(\@(\w)*: )|(\@(\w)*)*', '', string)
     string = re.sub('http://\S+|https://\S+', '', string)
-    emoji_pattern = re.compile("["
-                               u"\U0001F600-\U0001F64F"  # emoticons
-                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+    emoji_pattern = re.compile("["u"\U0001F600-\U0001F64F"
+                               u"\U0001F300-\U0001F5FF"
+                               u"\U0001F680-\U0001F6FF"
+                               u"\U0001F1E0-\U0001F1FF" 
                                u"\U00002702-\U000027B0"
                                u"\U000024C2-\U0001F251"
                                "]+", flags=re.UNICODE)
@@ -40,7 +34,7 @@ def translatorTweet(tweet):
 ### Database Connection
 client= MongoClient("mongodb+srv://dbUser:dbUser@cluster0.lcwhz.mongodb.net/ProgReti?retryWrites=true&w=majority")
 db=client['ProgReti']
-collection=db['Tweet']
+collection=db['Week-one']
 
 #### Twitter Developer Credentials
 consumerKey = '577Wyn7YJ15g9QreB4FhtdeFv'
@@ -55,18 +49,14 @@ data = {"documents": []}
 tweetSentiment = {"documents": []}
 
 # Inserimento Hashtag
-searchTerm = input("Enter Keyword/Tag to search about: ")
+inputTerm = input("Enter Keyword/Tag to search about: ")
+searchTerm = inputTerm + " -filter:retweets"
 analyzer = SentimentIntensityAnalyzer()
 rt_count = 0
 
 print("Start Tweet Capturing..")
-for tweet in tweepy.Cursor(api.search,q=searchTerm, lang="it", since="2020-11-07", until="2020-11-14", result_type="mixed", tweet_mode="extended").items(200):
-    if tweet.full_text.startswith("RT @") == True:
-        tweet.full_text = format_tweet(tweet.full_text)
-        retweet_status = True
-        rt_count += 1
-    else:
-        retweet_status = False
+for tweet in tweepy.Cursor(api.search,q=searchTerm, lang="it", since="2020-11-07", until="2020-11-14", result_type="mixed", tweet_mode="extended").items():
+    rt_count = tweet.retweet_count
     print(tweet.created_at, tweet.full_text)
     p2 = ["", tweet.full_text]
     hast = ""
@@ -88,24 +78,17 @@ for tweet in tweepy.Cursor(api.search,q=searchTerm, lang="it", since="2020-11-07
                 hast+="#"+p1[0]
     ddata=datetime.datetime.strptime(str(tweet.created_at),'%Y-%m-%d %H:%M:%S')
     tweet.full_text = cleanTweet(tweet.full_text)
-    trans = translatorTweet(tweet.full_text)
-    sentiment = analyzer.polarity_scores(trans)
+    #trans = translatorTweet(tweet.full_text)
+    #sentiment = analyzer.polarity_scores(trans)
     tt={
-        "id": tweet.id,
-        'Hashtag': searchTerm,
-        "Retweet": retweet_status,
-        "data":str(ddata),
-        "testo":tweet.full_text,
-        "hashtags": hast,
-        "score": sentiment
+        "Id": tweet.id,
+        'Hashtag': inputTerm,
+        "Retweet": rt_count,
+        "Data":str(ddata),
+        "Testo":tweet.full_text,
+        "Hashtags": hast,
+        "Sentiment": 0
     }
-    data['documents'].append({"language": "it", "id": tweet.id, "text": tweet.full_text})
-    tweetSentiment['documents'].append(({"language": "it", "id": tweet.id, "text": trans, "score": sentiment}))
+    data['documents'].append({"language": "it", "id": tweet.id, "text": tweet.full_text, "Retweet": rt_count})
+    #tweetSentiment['documents'].append(({"language": "it", "id": tweet.id, "text": trans, "score": sentiment}))
     collection.insert_one(tt)
-
-
-with open('week.json', 'w', encoding='utf8') as outfile:
-    json.dump(data, outfile, ensure_ascii=False)
-
-with open('result.json', 'w', encoding='utf8') as outfile: #'a' per aggiungere alla fine
-    json.dump(tweetSentiment, outfile, ensure_ascii=False)
