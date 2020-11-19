45bf716b84cc41c53b278e5ed68c24001c4290cb
FrancescoPa96 <43400908+FrancescoPa96@users.noreply.github.com>
2020-11-19 22:39:45 +0100

    Fix funzione cleanTweet e Hashtags per rimuovere più emoji

--- TwitterCapture.py
+++ TwitterCapture.py
@@ -1,27 +1,64 @@
 import tweepy
-import json
 import pymongo
-import datetime
-import requests
-import os
-import re
+import datetime,requests,os,re
 from pymongo import MongoClient
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from deep_translator import MyMemoryTranslator
 
-
 ### Formattazione Tweets
 def cleanTweet(string):
-    string = string.replace('#', '')
+    string = string.strip()
+    string = string.replace('#', ' ')
     string = string.replace('\n', '')
     string = re.sub('(\@(\w)*: )|(\@(\w)*)*', '', string)
     string = re.sub('http://\S+|https://\S+', '', string)
-    emoji_pattern = re.compile("["u"\U0001F600-\U0001F64F"
-                               u"\U0001F300-\U0001F5FF"
-                               u"\U0001F680-\U0001F6FF"
-                               u"\U0001F1E0-\U0001F1FF" 
+    emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               u"\U00002500-\U00002BEF"  # chinese char
+                               u"\U00002702-\U000027B0"
                                u"\U00002702-\U000027B0"
                                u"\U000024C2-\U0001F251"
+                               u"\U0001f926-\U0001f937"
+                               u"\U00010000-\U0010ffff"
+                               u"\u2640-\u2642"
+                               u"\u2600-\u2B55"
+                               u"\u200d"
+                               u"\u23cf"
+                               u"\u23e9"
+                               u"\u231a"
+                               u"\ufe0f"  # dingbats
+                               u"\u3030"
+                               "]+", flags=re.UNICODE)
+    string = emoji_pattern.sub(r'', string)
+    return string
+
+### Formattazione Hashtags
+def cleanHashtags(string):
+    string = string.replace('\n', '')
+    string = re.sub('(\@(\w)*: )|(\@(\w)*)*', '', string)
+    string = re.sub('http://\S+|https://\S+', '', string)
+    emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               u"\U00002500-\U00002BEF"  # chinese char
+                               u"\U00002702-\U000027B0"
+                               u"\U00002702-\U000027B0"
+                               u"\U000024C2-\U0001F251"
+                               u"\U0001f926-\U0001f937"
+                               u"\U00010000-\U0010ffff"
+                               u"\u2640-\u2642"
+                               u"\u2600-\u2B55"
+                               u"\u200d"
+                               u"\u23cf"
+                               u"\u23e9"
+                               u"\u231a"
+                               u"\ufe0f"  # dingbats
+                               u"\u3030"
                                "]+", flags=re.UNICODE)
     string = emoji_pattern.sub(r'', string)
     return string
@@ -31,16 +68,17 @@ def translatorTweet(tweet):
     translated = MyMemoryTranslator(source="it", target="en").translate(tweet)
     return translated
 
+
 ### Database Connection
 client= MongoClient("mongodb+srv://dbUser:dbUser@cluster0.lcwhz.mongodb.net/ProgReti?retryWrites=true&w=majority")
 db=client['ProgReti']
 collection=db['Week-one']
 
 #### Twitter Developer Credentials
-consumerKey = '577Wyn7YJ15g9QreB4FhtdeFv'
-consumerSecret = 'HEHaDmj4gpmEtZdcVpXeA4KYRcMDwMqpaHYLdrP6ui2o25NtA9'
-accessToken = '1327570641617362945-a3By7auPR9L6jmvFRoqfzJ9BLMILWE'
-accessTokenSecret = 'Ltl1db5rS52Yukcozmjlpiok4JVDTp4i411JMT1m4oPtQ'
+consumerKey = 'bBL43Fu0ZcXCxxnzOhCmqxPu9'
+consumerSecret = 'begHo3PEX199EuegKzqzmzcYjcV2hcvJxI0cYBdSLeEzPyR48Y'
+accessToken = '718840068-lGh9WG0qvvZ7Au3ar0lrFAczzN0SVkzmzFqoKNgJ'
+accessTokenSecret = 'FyCzGpaCkC0MpHycHxhpYyBs7hgIrUQMh3EMG7UNHV2KF'
 auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
 auth.set_access_token(accessToken, accessTokenSecret)
 api = tweepy.API(auth,wait_on_rate_limit=True)
@@ -55,7 +93,7 @@ analyzer = SentimentIntensityAnalyzer()
 rt_count = 0
 
 print("Start Tweet Capturing..")
-for tweet in tweepy.Cursor(api.search,q=searchTerm, lang="it", since="2020-11-07", until="2020-11-14", result_type="mixed", tweet_mode="extended").items():
+for tweet in tweepy.Cursor(api.search,q=searchTerm, lang="it", since="2020-11-11", until="2020-11-19", result_type="mixed", tweet_mode="extended").items():
     rt_count = tweet.retweet_count
     print(tweet.created_at, tweet.full_text)
     p2 = ["", tweet.full_text]
@@ -78,6 +116,7 @@ for tweet in tweepy.Cursor(api.search,q=searchTerm, lang="it", since="2020-11-07
                 hast+="#"+p1[0]
     ddata=datetime.datetime.strptime(str(tweet.created_at),'%Y-%m-%d %H:%M:%S')
     tweet.full_text = cleanTweet(tweet.full_text)
+    hast = cleanHashtags(hast)
     #trans = translatorTweet(tweet.full_text)
     #sentiment = analyzer.polarity_scores(trans)
     tt={
